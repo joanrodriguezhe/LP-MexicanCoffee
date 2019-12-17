2511dcb19869fce9643f61f0f15297babbd79f4f
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2019-12-17 12:04:12 -0700

    FIX: Express

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/public',express.static(path.join(__dirname, '/public')));
 
 app.use('', routes);
 
